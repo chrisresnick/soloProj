587c843d3061edb139182992efff36a31b742912
Chris Resnick <chris.resnick@gmail.com>
2020-11-19 16:44:38 -0500

    login 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage"
+import * as sessionActions from "./store/session";
 
 function App() {
-  return (
-    <Switch>
-      <Route path="/login" component={LoginFormPage} />
-    </Switch>
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() =>
+    setIsLoaded(true));
+  }, [dispatch]);
+
+  return isLoaded && (
+    <>
+      <h1>Home Page </h1>
+      <Switch>
+        <Route path="/login" component={LoginFormPage} />
+      </Switch>
+    </>
   );
 }
 
