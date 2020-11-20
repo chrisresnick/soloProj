7a8c861e9be7c7df95f526e33e432894dc23074d
Chris Resnick <chris.resnick@gmail.com>
2020-11-20 14:17:35 -0500

    listing seed

---
+++ backend/db/seeders/20201120190437-listings.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const {User} = require("../models/")
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+      await queryInterface.bulkInsert('Listings', [
+        {
+         title: "Great Falls Climbing Trip",
+         seller: (await User.findOne({where: {username: 'Demo-lition'}})).id,
+         description: "A day of climbing at Great Falls",
+         photo: "https://upload.wikimedia.org/wikipedia/commons/3/33/Great_Falls_National_Park_-_Lost_Arrow_%285.10%29_-_2.JPG",
+         price: 500.00,
+         createdAt: new Date(),
+         updatedAt: new Date(),
+        }
+  ], {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+     await queryInterface.bulkDelete('Listings', null, {});
+
+  }
+};
