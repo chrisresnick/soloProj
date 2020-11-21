db6051038568ee1bccd3c59ac30de7a1b7dc2ad7
Chris Resnick <chris.resnick@gmail.com>
2020-11-20 19:16:48 -0500

    layout listing still

--- frontend/src/components/Listing/index.js
+++ frontend/src/components/Listing/index.js
@@ -21,19 +21,27 @@ const Listing = () => {
         })()
     }, [params])
 
+    const adjustSelected = (num) =>  {
+        let newSelected = selectedImg + num;
+        if(newSelected < 0) newSelected = imgs.length-1;
+        if(newSelected >= imgs.length) newSelected  = 0;
+        setSelectedImg(newSelected);
+        console.log(selectedImg);
+    }
+
     return !loading && (
        <>
         <div className="disp">
             <div className="imgs">
                 <div className ="imgHolder">
-                    <i class="fas fa-chevron-up"></i>
+                    <i class="fas fa-chevron-up" onClick={() => adjustSelected(-1)}></i>
                     {imgs.map((imig, idx) => <img key={idx} onClick={() => setSelectedImg(idx)} src={imig} alt={listing.title}/>)}
-                    <i class="fas fa-chevron-down"></i>
+                    <i class="fas fa-chevron-down" onClick={() => adjustSelected(1)}></i>
                 </div>
                 <div class="main-img-holder">
-                    <i class="fas fa-chevron-left"></i>
+                    <i class="fas fa-chevron-left" onClick={() => adjustSelected(-1)}></i>
                     <img className="main-img" src={imgs[selectedImg]} alt={listing.title}/>
-                    <i class="fas fa-chevron-right"></i>
+                    <i class="fas fa-chevron-right" onClick={() => adjustSelected(1)}></i>
                 </div>
 
             </div>
