b4a0a1224a482eb27df4ed4e58febce3a20d3338
Chris Resnick <chris.resnick@gmail.com>
2020-11-23 14:16:08 -0500

    cart listing in progress

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useSelector} from "react-redux";
+import fetch from "../../store/csrf";
+import Item from "./Item";
 
 
 const Cart = () => {
+    const [cart, setCart] = useState([])
+    const {id} = useSelector(state => state.session.user);
+    const [cartIsRefreshed, setCartIsRefreshed] = useState(false);
+
+    useEffect(() => {
+        if(cartIsRefreshed) return;
+        (async () => {
+            const res = await fetch(`/api/cart/${id}`);
+            setCart(res.data.cart);
+        })()
+        setCartIsRefreshed(true);
+        return () => setCartIsRefreshed(false);
+    })
+
+    const html = !cartIsRefreshed ? (<h1>Loading Cart...</h1>) : (
+        <>
+        {cart.map(item => <Item key={item.id} item={item} />)}
+        </>
+    );
+
     return (
-        <h1>Test</h1>
+        {html}
     );
 }
 
