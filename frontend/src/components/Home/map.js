c09f9c6e4d39289c9fc861edc5574a203a3581e1
Chris Resnick <chris.resnick@gmail.com>
2020-11-24 01:43:45 -0500

    set up maps

--- frontend/src/components/Home/map.js
+++ frontend/src/components/Home/map.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, useGoogleMap } from '@react-google-maps/api';
 
 const Map = (props) => {
-    const center = { lat: 39.8283, lng: -98.5795 }
+    const map = useGoogleMap();
 
+    const center = { lat: 39.8283, lng: -98.5795 }
     const containerStyle = {
         width: '60vw',
         height: '60vh'
     };
+
+
+
     return (
         <LoadScript googleMapsApiKey={"AIzaSyB7RL1VTdvSotBJ1RO6ZpHzQkb1jcnjTxA"}>
             <GoogleMap
