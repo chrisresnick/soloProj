3657ebe20df9c37ef883c415d86578870421646e
Chris Resnick <chris.resnick@gmail.com>
2020-11-24 02:32:35 -0500

    maps in progress

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,11 +1,23 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Map from "./map"
+import {fetch} from "../../store/csrf";
 import "./style.css"
 
+
 const Home = () => {
+
+    const [listings, setListings] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const {data} = await fetch("api/listings/");
+            setListings(data);
+        })()
+    }, []);
+
     return (
         <div className="home-holder">
-            <Map />
+            <Map listings={listings}/>
         </div>
     );
 }
--- frontend/src/components/Home/map.js
+++ frontend/src/components/Home/map.js
@@ -1,16 +1,14 @@
-import React from "react";
+import React, {useState useEffect} from "react";
 import { GoogleMap, LoadScript, useGoogleMap } from '@react-google-maps/api';
 
-const Map = (props) => {
-    const map = useGoogleMap();
 
+const Map = (props) => {
+    const [places, setPlaces]= useState({});
     const center = { lat: 39.8283, lng: -98.5795 }
     const containerStyle = {
         width: '60vw',
         height: '60vh'
     };
-
-
 
     return (
         <LoadScript googleMapsApiKey={"AIzaSyB7RL1VTdvSotBJ1RO6ZpHzQkb1jcnjTxA"}>
