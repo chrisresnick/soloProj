14c97f5845f3bb4acb2263c6cbcd2a9cdb46b782
Chris Resnick <chris.resnick@gmail.com>
2020-12-25 00:34:13 -0500

    create new listing backend

--- backend/routes/api/listings.js
+++ backend/routes/api/listings.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const asyncHadler = require("express-async-handler");
 
-const {Listing, ExtraPhoto, User, Review} = require("../../db/models")
+const {Listing, ExtraPhoto, User, Review, Coordinate} = require("../../db/models")
 const router = express.Router();
 
 const avrgRating = (ratings) => {
     const sum = ratings.reduce((acc, rating) => acc+rating.rating, 0)
     return ratings.length ? sum/ratings.length : 5;
 }
+
+router.put("/", asyncHadler(async (req, res) => {
+    console.log("body", req.body);
+    const seller = req.body.user;
+    const title = req.body.listingName;
+    const description = req.body.listingDescription;
+    const priceCents = req.body.price;
+    const photo = req.body.photos[0];
+    const extraPhotos = req.body.photos.slice(1);
+    const {lat, long} = req.body;
+    const listing = await Listing.create({seller, title, description, photo, priceCents});
+    const coord = await Coordinate.create({listingId: listing.id, latitude: lat, longitude: long});
+    extraPhotos.forEach(async photo => await ExtraPhoto.create({listingId: listing.id, photo}));
+    return res.json({id:listing.id})
+}));
 
 router.get("/:id(\\d+)", asyncHadler(async (req, res) => {
     const id = Number.parseInt(req.params.id);
@@ -18,6 +33,6 @@ router.get("/:id(\\d+)", asyncHadler(async (req, res) => {
     json.User.rating = avrgRating(json.User.received)
     return res.json(json);
 
-}))
+}));
 
 module.exports = router;
--- frontend/src/components/NewListing/index.js
+++ frontend/src/components/NewListing/index.js
@@ -1,15 +1,20 @@
 import React, {useState} from "react";
+import {useSelector} from "react-redux";
+import {useHistory} from "react-router-dom";
 import AddPhoto from "./addPhoto";
 import PhotoDisplay from "./photoDisplay";
-import "./newListing.css"
+import {fetch} from "../../store/csrf";
+import "./newListing.css";
 
 const NewListing = () => {
     const [listingName, setListingName] = useState("");
-    const [listingDesription, setListingDescription] = useState("");
+    const [listingDescription, setListingDescription] = useState("");
     const [price, setPrice] = useState("");
     const [lat, setLat] = useState("")
     const [long, setLong] = useState("")
     const [photos, setPhotos] = useState([]);
+    const user = useSelector(state => state.session.user);
+    const history = useHistory();
 
     const addToPhotos = (url) => {
         setPhotos([...photos, url])
@@ -19,13 +24,27 @@ const NewListing = () => {
         setPhotos(photos.filter(photo => photo != url));
     }
 
+    const submit = async (e) => {
+        e.preventDefault();
+        const res = await fetch("/api/listings", {
+            method: "PUT",
+            body: JSON.stringify({
+                listingName, listingDescription, price: price*100, lat, long, photos, user: user.id,
+            })
+        })
+        if(res.data.id){
+            history.push(`/listings/${res.data.id}`);
+        }
+
+    }
+
     return (
         <div className="new-listing-holder">
             <h1>Create New Listing</h1>
             <form className="new-listing-form">
                 <input placeholder="Listing Name" value={listingName} onChange={e=>setListingName(e.target.value)}></input>
                 <input placeholder="Price" type="number" value={price} onChange={e=>setPrice(e.target.value)}></input>
-                <textarea placeholder="Description" value={listingDesription} onChange={e=>setListingDescription(e.target.value)}></textarea>
+                <textarea placeholder="Description" value={listingDescription} onChange={e=>setListingDescription(e.target.value)}></textarea>
                 <fieldset className="coords">
                     <legend>Coordinates:</legend>
                     <input placeholder="Latitude" value={lat} onChange={e=>setLat(e.target.value)}></input>
@@ -38,7 +57,7 @@ const NewListing = () => {
                     </div>
                     <AddPhoto addToPhotos={addToPhotos}/>
                 </fieldset>
-                <button className="submit-button">Add New Listing</button>
+                <button onClick={submit}className="submit-button">Add New Listing</button>
 
             </form>
         </div>
