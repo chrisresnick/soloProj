b3bce5c05917ef9db3b0ad1754c80a6fe64cf4e0
Christoher Resnick <chris.resnick@gmail.com>
2021-02-01 12:37:33 -0500

    work on nav bar styling to fit icons

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -15,19 +15,21 @@ const Navigation = ({isLoaded}) => {
         dispatch(sessionActions.logout());
     }
     const links = user ? (
-        <>
         <li>
-            <i className="fas fa-sign-out-alt" onClick={logout}></i>
+            <ul className="nav-links-ul">
+                <li>
+                    <i className="fas fa-sign-out-alt" onClick={logout}></i>
+                </li>
+                <li>
+                    <i className="far fa-plus-square"
+                    onClick={e => history.push("/newListing")}></i>
+                </li>
+                <li>
+                    <i  className="fas fa-shopping-cart"
+                        onClick={() => history.push("/cart")}></i>
+                </li>
+            </ul>
         </li>
-        <li>
-            <i className="far fa-plus-square"
-            onClick={e => history.push("/newListing")}></i>
-        </li>
-        <li>
-            <i  className="fas fa-shopping-cart"
-                onClick={() => history.push("/cart")}></i>
-        </li>
-        </>
     ) : (
         <>
           <li><i className="fas fa-sign-in-alt" onClick={() => dispatch(requireActions.setRequireLogin(true))}></i></li>
